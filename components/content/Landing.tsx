f911d49b6a5c730c0c1adbbb6e451dec0a4d1cef
Hamza-ben-yaflah <benyaflah0hamza@gmail.com>
2022-02-08 08:41:08 +0100

    add hero section

--- components/content/Landing.tsx
+++ components/content/Landing.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import Image from "next/image";
-
 import styles from "./Landing.module.css";
+import { Row, Col, Button, Space } from "antd";
+import { Typography } from "antd";
 
+const { Title, Text } = Typography;
 const Landig = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div>
+      <Space direction="vertical" size="large">
+        <div className={styles.container}>
+          <section className={styles.wraper}>
+            <Title style={{ color: "white" }} underline>
+              BUY.SEll.RENT
+            </Title>
+            <Text style={{ color: "white" }}>
+              BSRcars make it easy for you to choose your car
+            </Text>
+          </section>
+        </div>
+      </Space>
+    </div>
+  );
 };
 
 export default Landig;
