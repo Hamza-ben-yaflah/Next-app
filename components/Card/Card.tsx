f0d9195b0841f493b34538518005069456c836c2
Hamza-ben-yaflah <benyaflah0hamza@gmail.com>
2022-02-12 17:43:23 +0100

    Refactor Card component

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,24 +1,30 @@
 import React from "react";
 import { Typography, Button } from "antd";
 import styles from "./card.module.css";
-interface CardProp {
-  title: string;
-  description: string;
-  Click: string;
-}
+import Link from "next/link";
+// interface CardProp {
+//   title: string;
+//   description: string;
+//   Click: string;
+//   LinkTo: string;
+// }
 
 const { Title, Paragraph } = Typography;
 
-function Card({ title, description, Click }: CardProp) {
+function Card({ card }: any) {
+  console.log(card);
+
   return (
     <div className={styles.card}>
       <Title level={4} italic>
-        {title}
+        {card.fields.title}
       </Title>
-      <Paragraph>{description}</Paragraph>
-      <Button type="primary" size="large">
-        {Click}
-      </Button>
+      <Paragraph>{card.fields.descreption}</Paragraph>
+      <Link href={card.fields.button.fields.url} passHref>
+        <Button type="primary" size="large">
+          {card.fields.button.fields.title}
+        </Button>
+      </Link>
     </div>
   );
 }
