46dc2442db9afe9920c11f6cbbec80c0e1a50bdd
Hamza-ben-yaflah <benyaflah0hamza@gmail.com>
2022-02-17 10:08:47 +0100

    Create ContainerBuy Component

---
+++ components/ContainerBuy/ContainerBuy.tsx
@@ -0,0 +1,16 @@
+import { Col, Row } from "antd";
+import React from "react";
+import SideBar from "../SideBar/SideBar";
+
+const ContainerBuy = () => {
+  return (
+    <Row>
+      <Col lg={6}>
+        <SideBar />
+      </Col>
+      <Col lg={18}>sdf s</Col>
+    </Row>
+  );
+};
+
+export default ContainerBuy;
