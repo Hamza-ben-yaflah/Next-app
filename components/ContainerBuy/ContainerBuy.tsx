81a419074c078140afd6e382a911a77617ee2e01
Hamza-ben-yaflah <benyaflah0hamza@gmail.com>
2022-02-17 12:56:22 +0100

    Refactor ContainerBuy component

--- components/ContainerBuy/ContainerBuy.tsx
+++ components/ContainerBuy/ContainerBuy.tsx
@@ -1,14 +1,17 @@
 import { Col, Row } from "antd";
 import React from "react";
+import CardCarsContainer from "../CardCarsContainer/CardCarsContainer";
 import SideBar from "../SideBar/SideBar";
 
-const ContainerBuy = () => {
+const ContainerBuy = ({ cars }: { cars: any }) => {
   return (
     <Row>
       <Col lg={6}>
         <SideBar />
       </Col>
-      <Col lg={18}>sdf s</Col>
+      <Col lg={18}>
+        <CardCarsContainer cars={cars} />
+      </Col>
     </Row>
   );
 };
